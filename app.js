7e50ad94f40a4b996d135d77b46635de0f7d4453
Katherina Ayelen Juarez <katherinajuarez09@gmail.com>
2024-06-28 11:05:23 -0300

    Quinto commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
   });
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor corriendo con éxito en puerto ${port}`));
 
 
