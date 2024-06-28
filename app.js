87a978207d3acbf6e992c0b37d754667b163db00
Katherina Ayelen Juarez <katherinajuarez09@gmail.com>
2024-06-28 09:47:35 -0300

    Primer commit del proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+  });
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Servidor corriendo con éxito en puerto ${port}`));
+
+
